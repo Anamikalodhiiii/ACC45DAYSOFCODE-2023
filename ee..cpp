359591db60f7fcfcc8ae10dd34b8317d90bc921a
Anamikalodhiiii <145283622+Anamikalodhiiii@users.noreply.github.com>
2023-09-22 10:16:18 -0700

    Add files via upload

---
+++ ee..cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("enter an integer number\n");
+	scanf("%d",&number);
+	
+	if (number<=15)
+	printf("yes\n\n");
+	else
+	printf("no\n");
+	return(0);
+}
